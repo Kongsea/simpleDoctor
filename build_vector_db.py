885e7beb956eb10e8dae1e341bc4c8922d16ad0e
Kongsea <kongsea@gmail.com>
2024-01-11 20:54:34 +0800

    first commit

---
+++ build_vector_db.py
@@ -0,0 +1,15 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+import os
+
+# 定义 Embeddings
+embeddings = HuggingFaceEmbeddings(model_name="/root/data/model/sentence-transformer")
+
+# 向量数据库持久化路径
+persist_directory = 'data_base/vector_db/diy'
+
+# 加载数据库
+vectordb = Chroma(
+    persist_directory=persist_directory, 
+    embedding_function=embeddings
+)
